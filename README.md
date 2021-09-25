42a40ce3a1c41e5e6d4763c184baebc418728a9d
王朝孟 <wangchaomeng@weiweidagong.com>
2021-09-26 00:23:51 +0800

    java web 学习 第一弹

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JavaWebSpace
 java web 学习
+
+#### Servlet01 
+java web 基础、tomcat 配置，简单Servlet使用
+
+#### Servlet02
+
+
+
+
+
---
+++ Servlet01/src/main/java/HelloServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+
+        System.out.println("进入 doGet 方法 。。");
+
+        // 这里需要提前设置编码（如果对象创建再设置编码就没什么用了）
+        resp.setContentType("text/html;charset=gbk");
+
+        PrintWriter writer = resp.getWriter();
+        writer.print("你好，， nihao,,,servlet ..... ");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        doGet(req,resp);
+    }
+}
