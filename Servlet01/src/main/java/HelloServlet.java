2f442b695a7e376dc1811b5f501844fab638cfe1
王朝孟 <wangchaomeng@weiweidagong.com>
2021-09-30 17:09:49 +0800

    修改编码方式代码提交

--- Servlet01/src/main/java/HelloServlet.java
+++ Servlet01/src/main/java/HelloServlet.java
@@ -8,12 +8,11 @@ import java.io.PrintWriter;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doGet(req, resp);
 
         System.out.println("进入 doGet 方法 。。");
 
         // 这里需要提前设置编码（如果对象创建再设置编码就没什么用了）
-        resp.setContentType("text/html;charset=gbk");
+        resp.setContentType("text/html;charset=utf-8");
 
         PrintWriter writer = resp.getWriter();
         writer.print("你好，， nihao,,,servlet .....RRRRRRRR ");
@@ -21,7 +20,6 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doPost(req, resp);
         doGet(req,resp);
     }
 }
